00f1536bc00a7c31569cbc1e7b9660f3eef6363f
kevin <kevin.ulreich@epitech.eu>
2022-01-20 18:09:33 +0100

    TP08

---
+++ tp08_creationUtilisateur.js
@@ -0,0 +1,40 @@
+function load() {
+    let tbody = document.querySelector("tbody")
+    let data;
+    
+    fetch('https://digicode.cleverapps.io/utilisateurs')
+    .then(response => response.json())
+    .then(res => {
+        res.map(r=> {
+            data += `<tr><td>${r.nom}</td><td>${r.prenom}</td><td>${r.dateNaissance}</td><td>${r.lieuNaissance}</td><td>${r.departementNaissance}</td><td>${r.numeroRue}</td><td>${r.libelleRue}</td><td>${r.codePostal}</td><td>${r.ville}</td>`; 
+            console.log(r)
+        })
+        tbody.innerHTML = data;
+    });
+}
+    function valider() {
+        let obj = {};
+        obj.nom = document.querySelector("#nom").value;
+        obj.prenom = document.querySelector("#prenom").value;
+        obj.dateNaissance = document.querySelector("#dateNaissance").value;
+        obj.lieuNaissance = document.querySelector("#lieuNaissance").value;
+        obj.departementNaissance = document.querySelector("#departement").value;
+        obj.numeroRue = document.querySelector("#numeroRue").value;
+        obj.libelleRue = document.querySelector("#libelle").value;
+        obj.codePostal = document.querySelector("#codePostal").value;
+        obj.ville = document.querySelector("#ville").value;
+
+        console.log(obj)
+        fetch("https://digicode.cleverapps.io/utilisateurs", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(obj)
+          }).then((response) => {
+       response.text();
+        }).then((data) => {
+          console.log(data)
+        });
+    }
+
